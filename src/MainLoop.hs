85123a7c82e6045076f364ae10c41b0e2d614aba
Ruud van Asseldonk <ruud@channable.com>
2017-07-27 20:23:12 +0200

    Add Put type and state var with queue

--- src/MainLoop.hs
+++ src/MainLoop.hs
@@ -1,4 +1,25 @@
 module MainLoop (mainLoop) where
 
-mainLoop :: IO ()
-mainLoop = putStrLn "hi"
+import Control.Concurrent.STM.TVar (TVar, newTVarIO)
+import Control.Concurrent.STM.TBQueue (TBQueue, newTBQueueIO)
+import Data.Aeson (Value (..))
+import Data.Void (Void)
+import Data.Text (Text)
+
+-- Put is a command to put a value at a given path.
+data Put = Put [Text] Value
+
+data State = State
+  { stValue :: TVar Value
+  , stQueue :: TBQueue Put
+  }
+
+newState :: IO State
+newState = do
+  tvalue <- newTVarIO Null
+  tqueue <- newTBQueueIO 128
+  pure (State tvalue tqueue)
+
+mainLoop :: State -> IO Void
+mainLoop state =
+  mainLoop state
