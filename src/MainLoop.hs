78a4a8e593417d9f5ed60d4db7c1c4bc9ef992c0
Ruud van Asseldonk <ruud@channable.com>
2017-07-27 20:48:50 +0200

    Add handlePut function to update the current state

--- src/MainLoop.hs
+++ src/MainLoop.hs
@@ -5,6 +5,9 @@ import Control.Concurrent.STM.TBQueue (TBQueue, newTBQueueIO)
 import Data.Aeson (Value (..))
 import Data.Void (Void)
 import Data.Text (Text)
+import Data.Maybe (fromMaybe)
+
+import qualified Data.HashMap.Strict as HashMap
 
 -- Put is a command to put a value at a given path.
 data Put = Put [Text] Value
@@ -19,6 +22,19 @@ newState = do
   tvalue <- newTVarIO Null
   tqueue <- newTBQueueIO 128
   pure (State tvalue tqueue)
+
+-- Execute a "put" operation.
+handlePut :: Put -> Value -> Value
+handlePut (Put path newValue) value = case path of
+  [] -> newValue
+  key : pathTail ->
+    let
+      putInner = handlePut (Put pathTail newValue)
+      newDict = case value of
+        Object dict -> HashMap.alter (Just . putInner . fromMaybe Null) key dict
+        _notObject  -> HashMap.singleton key (putInner Null)
+    in
+      Object newDict
 
 mainLoop :: State -> IO Void
 mainLoop state =
