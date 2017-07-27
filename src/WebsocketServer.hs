eba3d6326a35a6d1eea7fc0dc10d12cd4054ca6b
Robert Kreuzer <robert@channable.com>
2017-07-27 23:14:29 +0200

    Parse incoming messages
    Store the path that they want to subscribe to, disconnect them if they
    send an invalid message.

--- src/WebsocketServer.hs
+++ src/WebsocketServer.hs
@@ -6,13 +6,17 @@ import Data.Monoid (mappend, (<>))
 import Data.Text (Text)
 import Control.Exception (finally)
 import Control.Monad (forM_, forever)
-import Control.Concurrent (MVar, modifyMVar)
+import Control.Concurrent (MVar, modifyMVar, readMVar)
 
 import qualified Network.WebSockets as WS
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-type Client = (Text, WS.Connection)
+-- the path that a client subscribes to, e.g. imports/1/status
+type Path = Text
+
+-- a client subscribes to a specific path, for which it will receive updates
+type Client = (Path, WS.Connection)
 
 type ServerState = [Client]
 
@@ -22,6 +26,8 @@ newServerState = []
 addClient :: Client -> ServerState -> ServerState
 addClient client clients = client : clients
 
+-- TODO: Multiple clients can subscribe to the same path,
+-- we need to define identity for a WS.Connection and filter on that
 removeClient :: Client -> ServerState -> ServerState
 removeClient client = filter ((/= fst client) . fst)
 
@@ -37,24 +43,43 @@ onConnect state pending = do
     -- accept the connection
     -- TODO: Validate the path and headers of the pending request
     conn <- WS.acceptRequest pending
-    let client = ("bla2", conn)
     -- fork a pinging thread, because browsers...
     WS.forkPingThread conn 30
 
-    -- add the connection to the server state
-    s <- modifyMVar state $ \s ->
-        let s' = addClient client s in return (s', s')
-
-    broadcast ("Number of clients: " `mappend` (T.pack $ show $ length s)) s
+    -- broadcast ("Number of clients: " `mappend` (T.pack $ show $ length s)) s
 
     flip finally (onDisconnect ("bla", conn) state) $ do
-        acceptMessages conn
+        acceptMessages conn state
 
-acceptMessages :: WS.Connection -> IO ()
-acceptMessages conn = forever $ do
+acceptMessages :: WS.Connection -> MVar ServerState -> IO ()
+acceptMessages conn state = do
     -- TODO: Come up with a wire protocol and validate it
     msg <- WS.receiveData conn
+    let parsedMessage = parseMessage msg
     T.putStrLn $ "Received message: " <> msg
+
+    case parsedMessage of
+      -- TODO: Disconnect the client
+      Nothing -> T.putStrLn $ "Invalid message: " <> msg
+      Just path -> do
+        T.putStrLn $ "Parsed path: " <> path
+        -- add the connection to the server state
+        let client = (path, conn)
+        _ <- modifyMVar state $ \s ->
+          let s' = addClient client s in return (s', s')
+        pushUpdates conn state client
+
+pushUpdates :: WS.Connection -> MVar ServerState -> Client -> IO ()
+pushUpdates _conn state _client = forever $ do
+  _updates <- readMVar state
+  putStrLn "bla"
+
+parseMessage :: Text -> Maybe Path
+parseMessage msg = case msg of
+    _ | prefix `T.isPrefixOf` msg -> Just $ T.drop (T.length prefix) msg
+      | otherwise -> Nothing
+    where
+      prefix = "Path: "
 
 onDisconnect :: (Text, WS.Connection) -> MVar ServerState -> IO ()
 onDisconnect client state = do
