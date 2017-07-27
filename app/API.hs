4eafdff693b74227677ec5988f4d39b662211d14
Nuno Alexandre <nuno@channable.com>
2017-07-27 22:11:09 +0200

    Read PUT request's path and json body

--- app/API.hs
+++ app/API.hs
@@ -3,17 +3,26 @@
 import Web.Scotty
 import  Data.Text (Text)
 import qualified Network.Wai        as Wai
+import Data.Aeson (Value(..))
+import Network.HTTP.Types
 
 main :: IO ()
 main = scotty 3000 $ do
     get (regex "^") $
         request >>= json . statusFor . Wai.pathInfo
 
-    post "/status/:key" $ do
-        json ("{ status: ok }" :: Text)
+    put (regex "^") $ do
+      req <- request
+      payload <- jsonData
+      status $ mkStatus (setStatus (Wai.pathInfo req) payload) "Created"
+
 
 statusFor :: [Text] -> Text
 statusFor ["a"] = "{downloading}"
 statusFor ["a", "b"] = "{failed}"
 statusFor ["a", "b", "c"] = "{succeeded}"
 statusFor _ = "{dikke Kip}"
+
+setStatus :: [Text] -> Value -> Int
+setStatus ["a", "b"] _ = 201
+setStatus _ _ = 404
