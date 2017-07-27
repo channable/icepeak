25433ffba51328882f9d2b29d2ee585ed01588f8
Ruud van Asseldonk <ruud@channable.com>
2017-07-27 22:20:41 +0200

    Ensure that the main application does not quit

--- app/Icepeak.hs
+++ app/Icepeak.hs
@@ -1,13 +1,18 @@
 module Main where
 
+import Control.Concurrent (forkIO)
 import Control.Monad (void)
 import Data.Void (absurd)
-import Control.Concurrent (forkIO)
 
 import qualified Core
+
+loop :: IO ()
+loop = loop
 
 main :: IO ()
 main = do
   core <- Core.newCore
   void $ forkIO (absurd <$> Core.processPuts core)
   void $ forkIO (absurd <$> Core.processUpdates core)
+  -- TODO: Start servers instead.
+  loop
