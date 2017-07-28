5b44ab08b6120aab9c13c0b664353a24cc529edc
Robert Kreuzer <robert@channable.com>
2017-07-28 11:43:15 +0200

    Don't use a UUID for tests anymore
    Since we abstacted the type we can just use an Int for example.

--- tests/SubscriptionTreeSpec.hs
+++ tests/SubscriptionTreeSpec.hs
@@ -1,11 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module SubscriptionTreeSpec (spec) where
 
-import Data.Maybe (fromJust)
 import Test.Hspec (Spec, describe, it, shouldBe)
 import Subscription (SubscriptionTree (..), empty, subscribe)
 
-import qualified Data.UUID as UUID
 import qualified Data.HashMap.Strict as HM
 
 spec :: Spec
@@ -15,17 +13,17 @@ spec = do
     it "adds a client listening to the root when calling subscribe with []" $ do
       let
         path = []
-        uuid = fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000"
+        conn_id = 1 :: Int
         conn = "dummy connection" :: String
-        after = SubscriptionTree (HM.fromList [(uuid, conn)]) HM.empty
-      subscribe path uuid conn empty `shouldBe` after
+        after = SubscriptionTree (HM.fromList [(conn_id, conn)]) HM.empty
+      subscribe path conn_id conn empty `shouldBe` after
 
     it "adds a client listening to \"some\" when calling subscribe with [\"some\"]" $ do
       let
         path = ["some"]
-        uuid = fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000"
+        conn_id = 1 :: Int
         conn = "dummy connection" :: [Char]
         after = SubscriptionTree
                   HM.empty
-                  (HM.fromList [("some", SubscriptionTree (HM.fromList [(uuid,conn)]) HM.empty)])
-      subscribe path uuid conn empty `shouldBe` after
+                  (HM.fromList [("some", SubscriptionTree (HM.fromList [(conn_id,conn)]) HM.empty)])
+      subscribe path conn_id conn empty `shouldBe` after
