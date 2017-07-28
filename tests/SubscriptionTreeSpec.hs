6b0f7da5226797da6f9a958b0aab288e3cacc214
Robert Kreuzer <robert@channable.com>
2017-07-28 11:40:30 +0200

    Test subscribing to a channel
    Why is it so hard for humans to type properly nested trees??

--- tests/SubscriptionTreeSpec.hs
+++ tests/SubscriptionTreeSpec.hs
@@ -12,18 +12,20 @@ spec :: Spec
 spec = do
   describe "SubscriptionTree" $ do
 
---    it "adds a client when calling subscribe" $ do
---      let
---        path = ["some"]
---        uuid = fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000"
---        conn = "dummy connection" :: [Char]
---        after = SubscriptionTree HM.empty (HM.fromList "some" )
---      subscribe path uuid conn empty `shouldBe` after
-
-    it "adds a client when calling subscribe" $ do
+    it "adds a client listening to the root when calling subscribe with []" $ do
       let
         path = []
         uuid = fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000"
         conn = "dummy connection" :: String
         after = SubscriptionTree (HM.fromList [(uuid, conn)]) HM.empty
+      subscribe path uuid conn empty `shouldBe` after
+
+    it "adds a client listening to \"some\" when calling subscribe with [\"some\"]" $ do
+      let
+        path = ["some"]
+        uuid = fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000"
+        conn = "dummy connection" :: [Char]
+        after = SubscriptionTree
+                  HM.empty
+                  (HM.fromList [("some", SubscriptionTree (HM.fromList [(uuid,conn)]) HM.empty)])
       subscribe path uuid conn empty `shouldBe` after
