a80bfe10906ac14009a9129f860a22bf3c838e5a
Ruud van Asseldonk <ruud@channable.com>
2017-07-28 10:34:05 +0200

    Extract subscription tree into a module
    It is both simpler and more generic now!

---
+++ src/Subscription.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module Subscription
+(
+  SubscriptionTree,
+  broadcast,
+  empty,
+  subscribe,
+  unsubscribe,
+)
+where
+
+import Control.Monad (forM_)
+import Data.Aeson (Value)
+import Data.HashMap.Strict (HashMap)
+import Data.Hashable (Hashable)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+
+import qualified Data.HashMap.Strict as HashMap
+
+-- Keeps subscriptions in a tree data structure, so we can efficiently determine
+-- which clients need to be notified for a given update.
+data SubscriptionTree k v =
+  SubscriptionTree (HashMap k v) (HashMap Text (SubscriptionTree k v))
+  deriving (Functor)
+
+empty :: SubscriptionTree k v
+empty = SubscriptionTree HashMap.empty HashMap.empty
+
+subscribe :: (Eq k, Hashable k) => [Text] -> k -> v -> SubscriptionTree k v -> SubscriptionTree k v
+subscribe path subid subval (SubscriptionTree here inner) =
+  case path of
+    [] -> SubscriptionTree (HashMap.insert subid subval here) inner
+    key : pathTail ->
+      let
+        subscribeInner = subscribe pathTail subid subval
+        newInner = HashMap.alter (Just . subscribeInner . fromMaybe empty) key inner
+      in
+        SubscriptionTree here newInner
+
+unsubscribe :: (Eq k, Hashable k) => [Text] -> k -> SubscriptionTree k v -> SubscriptionTree k v
+unsubscribe path subid (SubscriptionTree here inner) =
+  case path of
+    [] -> SubscriptionTree (HashMap.delete subid here) inner
+    key : pathTail ->
+      let
+        unsubscribeInner = unsubscribe pathTail subid
+        newInner = HashMap.adjust unsubscribeInner key inner
+        -- TODO: Prune empty branches in the tree.
+      in
+        SubscriptionTree here newInner
+
+-- Invoke f for all subscribers to the path. The subscribers get passed the
+-- subvalue at the path that they are subscribed to.
+broadcast :: Monad m => (Value -> v -> m ()) -> [Text] -> Value -> SubscriptionTree k v -> m ()
+broadcast f path value (SubscriptionTree here inner) = do
+  case path of
+    -- When the path is empty, all subscribers that are "here" or at a deeper
+    -- level should receive a notification.
+    [] -> do
+      forM_ here (f value)
+      -- TODO: Trim the value to the key of the subscriber.
+      forM_ inner (broadcast f [] value)
+    key : pathTail -> case HashMap.lookup key inner of
+      Nothing -> pure ()
+      -- TODO: Extract the inner thing from the value as well; the client is not
+      -- subscribed to the top-level thing after all.
+      Just subs -> broadcast f pathTail value subs
--- src/WebsocketServer.hs
+++ src/WebsocketServer.hs
@@ -12,74 +12,36 @@ import Control.Concurrent (MVar, modifyMVar_, yield)
 import Control.Exception (finally)
 import Control.Monad (forM_, forever)
 import Data.Aeson (Value)
-import Data.HashMap.Strict (HashMap)
-import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import Data.UUID
 import System.Random (randomIO)
 
 import qualified Data.Aeson as Aeson
-import qualified Data.HashMap.Strict as HashMap
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import qualified Network.WebSockets as WS
 
-type ServerState = SubscriptionTree
+import Subscription (SubscriptionTree)
 
--- Keeps subscriptions in a tree data structure, so we can efficiently determine
--- which clients need to be notified for a given update.
-data SubscriptionTree =
-  SubscriptionTree (HashMap UUID WS.Connection) (HashMap Text SubscriptionTree)
+import qualified Subscription
 
-emptySubsTree :: SubscriptionTree
-emptySubsTree = SubscriptionTree HashMap.empty HashMap.empty
-
-subscribe :: [Text] -> UUID -> WS.Connection -> SubscriptionTree -> SubscriptionTree
-subscribe path uuid conn (SubscriptionTree here inner) =
-  case path of
-    [] -> SubscriptionTree (HashMap.insert uuid conn here) inner
-    key : pathTail ->
-      let
-        subscribeInner = subscribe pathTail uuid conn
-        newInner = HashMap.alter (Just . subscribeInner . fromMaybe emptySubsTree) key inner
-      in
-        SubscriptionTree here newInner
-
-unsubscribe :: [Text] -> UUID -> SubscriptionTree -> SubscriptionTree
-unsubscribe path uuid (SubscriptionTree here inner) =
-  case path of
-    [] -> SubscriptionTree (HashMap.delete uuid here) inner
-    key : pathTail ->
-      let
-        unsubscribeInner = unsubscribe pathTail uuid
-        newInner = HashMap.adjust unsubscribeInner key inner
-        -- TODO: Prune empty branches in the tree.
-      in
-        SubscriptionTree here newInner
+type ServerState = SubscriptionTree UUID WS.Connection
 
 newUUID :: IO UUID
 newUUID = randomIO
 
 newServerState :: ServerState
-newServerState = emptySubsTree
+newServerState = Subscription.empty
 
 broadcast :: [Text] -> Value -> ServerState -> IO ()
-broadcast path value (SubscriptionTree here inner) = do
-  case path of
-    -- When the path is empty, all subscribers that are "here" or at a deeper
-    -- level should receive a notification.
-    [] -> do
-      forM_ here $ \ conn -> do
-        putStrLn $ "Sending binary data ..."
-        WS.sendBinaryData conn (Aeson.encode value)
-      -- TODO: Trim the value to the key.
-      forM_ inner $ broadcast [] value
-    key : pathTail -> case HashMap.lookup key inner of
-      Nothing -> pure ()
-      -- TODO: Extract the inner thing from the value as well; the client is not
-      -- subscribed to the top-level thing after all.
-      Just subs -> broadcast pathTail value subs
+broadcast =
+  let
+    send value conn = do
+      putStrLn $ "Sending binary data ..."
+      WS.sendBinaryData conn (Aeson.encode value)
+  in
+    Subscription.broadcast send
 
 -- Called for each new client that connects.
 acceptConnection :: MVar ServerState -> WS.PendingConnection -> IO ()
@@ -102,10 +64,10 @@ handleFirstMessage conn state = do
     -- than an arbitrary end of buffer?
     path = filter (not . Text.null) $ Text.split (== '/') msg
     onConnect = do
-      modifyMVar_ state (pure . subscribe path uuid conn)
+      modifyMVar_ state (pure . Subscription.subscribe path uuid conn)
       putStrLn $ "Client " ++ (show uuid) ++ " connected, subscribed to " ++ (show path) ++ "."
     onDisconnect = do
-      modifyMVar_ state (pure . unsubscribe path uuid)
+      modifyMVar_ state (pure . Subscription.unsubscribe path uuid)
       putStrLn $ "Client " ++ (show uuid) ++ " disconnected."
     -- We don't send any messages here; sending is done by the update
     -- loop; it finds the client in the set of subscriptions. But we do
