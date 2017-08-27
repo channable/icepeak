736b3411b0d34e8111073b24d40cca131d44e875
Robert Kreuzer <robert@channable.com>
2017-08-27 18:59:05 +0200

    Add a readFile function
    This makes the main function much cleaner.

--- app/Icepeak.hs
+++ app/Icepeak.hs
@@ -5,7 +5,7 @@ module Main where
 import Control.Concurrent.Async
 import Control.Exception (try, SomeException)
 import Control.Monad (void)
-import Data.Aeson (eitherDecodeStrict)
+import Data.Aeson (eitherDecodeStrict, Value)
 import Data.ByteString (hGetContents, ByteString)
 import Options.Applicative (execParser)
 import Prelude hiding (log)
@@ -23,7 +23,7 @@ import qualified HttpServer
 import qualified Server
 import qualified WebsocketServer
 
--- Instal SIGTERM and SIGINT handlers to do a graceful exit.
+-- Install SIGTERM and SIGINT handlers to do a graceful exit.
 installHandlers :: Core -> Async () -> IO ()
 installHandlers core serverThread =
   let
@@ -38,30 +38,35 @@ installHandlers core serverThread =
     void $ installHandler Signals.sigTERM
     void $ installHandler Signals.sigINT
 
+
+readValue :: FilePath -> IO Value
+readValue filePath = do
+  eitherEncodedValue <- try $ withFile filePath ReadMode hGetContents
+  case (eitherEncodedValue :: Either SomeException ByteString) of
+      Left exc -> error $ "Failed to read the data from disk: " ++ show exc
+      Right encodedValue -> case eitherDecodeStrict encodedValue of
+        Left msg  -> error $ "Failed to decode the initial data: " ++ show msg
+        Right value -> return value
+
+
 main :: IO ()
 main = do
   config <- execParser configInfo
   -- load the persistent data from disk
   let filePath = configDataFile config
-  eitherEncodedValue <- try $ withFile filePath ReadMode hGetContents
+  value <- readValue filePath
+  core <- Core.newCore value config
+  httpServer <- HttpServer.new core
+  let wsServer = WebsocketServer.acceptConnection core
+  pops <- Async.async $ Core.processOps core
+  upds <- Async.async $ WebsocketServer.processUpdates core
+  serv <- Async.async $ Server.runServer wsServer httpServer
+  logger <- Async.async $ processLogRecords (coreLogRecords core)
+  installHandlers core serv
+  log "System online. ** robot sounds **" (coreLogRecords core)
 
-  case (eitherEncodedValue :: Either SomeException ByteString) of
-      Left exc -> putStrLn $ "Failed to read the data from disk: " ++ show exc
-      Right encodedValue -> case eitherDecodeStrict encodedValue of
-          Left msg  -> error $ "Failed to decode the initial data: " ++ show msg
-          Right value -> do
-              core <- Core.newCore value config
-              httpServer <- HttpServer.new core
-              let wsServer = WebsocketServer.acceptConnection core
-              pops <- Async.async $ Core.processOps core
-              upds <- Async.async $ WebsocketServer.processUpdates core
-              serv <- Async.async $ Server.runServer wsServer httpServer
-              logger <- Async.async $ processLogRecords (coreLogRecords core)
-              installHandlers core serv
-              log "System online. ** robot sounds **" (coreLogRecords core)
-
-              -- TODO: Log exceptions properly (i.e. non-interleaved)
-              void $ Async.wait pops
-              void $ Async.wait upds
-              void $ Async.wait serv
-              void $ Async.wait logger
+  -- TODO: Log exceptions properly (i.e. non-interleaved)
+  void $ Async.wait pops
+  void $ Async.wait upds
+  void $ Async.wait serv
+  void $ Async.wait logger
