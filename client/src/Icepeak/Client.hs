c9bd558b1acb5f4f56e5f4ee856c3b54e2b33e65
Radek Slupik <radek@channable.com>
2017-09-06 13:30:11 +0200

    Add Icepeak.Client low-level functions

--- client/src/Icepeak/Client.hs
+++ client/src/Icepeak/Client.hs
@@ -1,3 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Functions are exported in descending order of abstraction level.
 module Icepeak.Client
-  (
+  ( setAtLeafRequest
+  , requestPathForIcepeakPath
   ) where
+
+import Data.Aeson (ToJSON)
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+
+import qualified Data.Aeson as Aeson
+import qualified Data.Binary.Builder as Binary.Builder
+import qualified Data.ByteString.Lazy as ByteString.Lazy
+import qualified Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Types.URI as URI
+
+-- | Return a HTTP request for setting a value at the leaf of a path.
+setAtLeafRequest :: ToJSON a => [Text] -> a -> HTTP.Request
+setAtLeafRequest path leaf =
+  HTTP.defaultRequest
+    { HTTP.method = "PUT"
+    , HTTP.path = requestPathForIcepeakPath path
+    , HTTP.requestBody = HTTP.RequestBodyLBS (Aeson.encode leaf)
+    }
+
+-- | Return the request path for an Icepeak path.
+requestPathForIcepeakPath :: [Text] -> ByteString
+requestPathForIcepeakPath =
+  ByteString.Lazy.toStrict .
+    Binary.Builder.toLazyByteString .
+      URI.encodePathSegments
