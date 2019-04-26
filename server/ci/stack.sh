1f73277fd1de6ed454078e816d0c23aedb82c039
Ruud van Asseldonk <ruud@channable.com>
2019-04-26 15:17:33 +0200

    Yaml templates and Bash escapes are terrible

--- server/ci/stack.sh
+++ server/ci/stack.sh
@@ -7,4 +7,8 @@ set -e
 export STACK_ROOT="${PWD}/.stack-root"
 export STACK_WORK=".stack-work"
 
-stack --no-terminal "$@"
+# NOTE: We do not quote the $@ here, so if the arguments contain whitespace,
+# then this will do additional splitting. That is as intended: in the Concourse
+# vars section we can only provide string vars, not arrays, so we need to
+# provide the entire command line as a string and let Bash do the splitting.
+stack --no-terminal $@
