4c4e54e45f0cfff4aa0cf873669b4148c4d82d8f
Ruud van Asseldonk <ruud@channable.com>
2019-04-26 15:06:17 +0200

    Fix stack path variables, attempt #2

--- server/ci/stack.sh
+++ server/ci/stack.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-export STACK_ROOT=".stack-root"
+# NOTE: Stack expects STACK_ROOT to be an absolute path,
+# and STACK_WORK to be relative.
+export STACK_ROOT="${PWD}/.stack-root"
 export STACK_WORK=".stack-work"
 
 stack --no-terminal "$@"
