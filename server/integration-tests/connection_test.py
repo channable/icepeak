e6487a2c623638b540b707c895a97eac1fc31979
Robert Kreuzer <robert@channable.com>
2019-10-22 18:24:38 +0200

    Update connection_test.py to work with Python3.7

--- server/integration-tests/connection_test.py
+++ server/integration-tests/connection_test.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python2.7
-from __future__ import absolute_import, division, unicode_literals
+#!/usr/bin/env python3.7
+"""
+Test PUTing some data into Icepeak and getting it back over a websocket.
 
+Requires a running Icepeak instance.
+
+Requirements can be installed with: pip install requests websockets
+"""
+
+import asyncio
 import json
 import requests
-import websocket
+import websockets
 
 # 1. Put some data into icepeak over HTTP
 new_data = {'status': 'freezing'}
@@ -11,11 +18,16 @@ requests.put('http://localhost:3000/so/cool',
     json.dumps(new_data))
 
 # 2. Get the data back over a websocket
-conn = websocket.create_connection("ws://localhost:3000/so/cool")
-result = conn.recv()
-parsed_result = json.loads(result)
+async def hello(uri):
+    async with websockets.connect(uri) as websocket:
+        result = await websocket.recv()
 
-assert new_data == parsed_result, 'Input data: {} is different from output data: {}'.format(
-        new_data, parsed_result)
+        parsed_result = json.loads(result)
 
-print 'Initial data was successfully sent to client!'
+        assert new_data == parsed_result, 'Input data: {} is different from output data: {}'.format(
+            new_data, parsed_result)
+
+print('Initial data was successfully sent to client!')
+
+asyncio.get_event_loop().run_until_complete(
+    hello('ws://localhost:3000/so/cool'))
