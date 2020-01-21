8c94826aa24b7616cf8bf51a441d8d9e19193bbf
Laurens Duijvesteijn <laurens@channable.com>
2020-01-21 09:26:43 +0100

    nix: Add getPkgs parameter to default.nix
    This allows users to override what nixpkgs we start from. That's useful
    if users want to avoid having an explosion of different versions of
    Nixpkgs.
    
    I'm not sure if this does what we want, as I suspect it might be
    cumbersome to use. There might be a more idiomatic pattern for this. Is
    there way to allow users to pass in a fully evaluated pkgs argument?

--- server/default.nix
+++ server/default.nix
@@ -1,3 +1,10 @@
+{ # We expect getPkgs to be a lambda that gives us a package set. Example:
+  # the lambda in `nixpkgs/default.nix` without any arguments passed to it.
+  # That allows us to pass in config later, while allowing users to pass in
+  # their own version of Nixpkgs.
+  getPkgs ? import ../nix/nixpkgs.nix
+}:
+
 let
   config = {
     packageOverrides = pkgs: rec {
@@ -10,7 +17,7 @@ let
     };
   };
 
-  pkgs = import ../nix/nixpkgs.nix { inherit config; };
+  pkgs = getPkgs { inherit config; };
 in
   {
     icepeak-server = pkgs.haskellPackages.icepeak;
