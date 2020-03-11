181cb892e5168e2c9c18b7a1bc33076c2b080b8b
Robert Kreuzer <robert.kreuzer@gmail.com>
2020-03-11 21:46:22 +0100

    Release Icepeak version v0.7.2
    Changelog:
    
    - Upgraded Stack from LTS 14.23 to 15.3

--- server/server.nix
+++ server/server.nix
@@ -41,7 +41,7 @@
 
 mkDerivation {
   pname = "icepeak";
-  version = "0.7.1";
+  version = "0.7.2";
 
   # Opt in to hpack. We don't commit the cabal file in our repo currently.
   buildTools = [ hpack ];
