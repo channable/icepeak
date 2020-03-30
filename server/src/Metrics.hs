7a12907032673c673f1acf3ebea4eafcd3086167
Wander Hillen <wjw.hillen@gmail.com>
2020-03-30 14:24:01 +0200

    Add metrics with proper Prometheus naming scheme

--- server/src/Metrics.hs
+++ server/src/Metrics.hs
@@ -25,21 +25,25 @@ countHttpRequest method status httpRequestCounter = withLabel httpRequestCounter
 
 
 data IcepeakMetrics = IcepeakMetrics
-  { icepeakMetricsRequestCounter  :: HttpRequestCounter
-  , icepeakMetricsDataSize        :: Gauge
-  , icepeakMetricsJournalSize     :: Gauge
-  , icepeakMetricsDataWritten     :: Counter
-  , icepeakMetricsJournalWritten  :: Counter
-  , icepeakMetricsSubscriberCount :: Gauge
+  { icepeakMetricsRequestCounter    :: HttpRequestCounter
+  , icepeakMetricsDataSize          :: Gauge
+  , icepeakMetricsDataSizeBytes     :: Gauge
+  , icepeakMetricsJournalSize       :: Gauge
+  , icepeakMetricsDataWritten       :: Counter
+  , icepeakMetricsDataWrittenTotal  :: Counter
+  , icepeakMetricsJournalWritten    :: Counter
+  , icepeakMetricsSubscriberCount   :: Gauge
   }
 
 createAndRegisterIcepeakMetrics :: IO IcepeakMetrics
 createAndRegisterIcepeakMetrics = IcepeakMetrics
   <$> register (vector ("method", "status") requestCounter)
   <*> register (gauge (Info "icepeak_data_size" "Size of data file in bytes."))
+  <*> register (gauge (Info "icepeak_data_size_bytes" "Size of data file in bytes."))
   <*> register (gauge (Info "icepeak_journal_size_bytes"
                             "Size of journal file in bytes."))
   <*> register (counter (Info "icepeak_data_written" "Total number of bytes written so far."))
+  <*> register (counter (Info "icepeak_data_written_bytes_total" "Total number of bytes written so far."))
   <*> register (counter (Info "icepeak_journal_written_bytes_total"
                               "Total number of bytes written to the journal so far."))
   <*> register (gauge
@@ -52,7 +56,9 @@ notifyRequest :: Http.Method -> Http.Status -> IcepeakMetrics -> IO ()
 notifyRequest method status = countHttpRequest method status . icepeakMetricsRequestCounter
 
 setDataSize :: (MonadMonitor m, Real a) => a -> IcepeakMetrics -> m ()
-setDataSize val metrics = setGauge (icepeakMetricsDataSize metrics) (realToFrac val)
+setDataSize val metrics = do
+  setGauge (icepeakMetricsDataSize      metrics) (realToFrac val)
+  setGauge (icepeakMetricsDataSizeBytes metrics) (realToFrac val)
 
 setJournalSize :: (MonadMonitor m, Real a) => a -> IcepeakMetrics -> m ()
 setJournalSize val metrics = setGauge (icepeakMetricsJournalSize metrics) (realToFrac val)
@@ -60,8 +66,9 @@ setJournalSize val metrics = setGauge (icepeakMetricsJournalSize metrics) (realT
 -- | Increment the total data written to disk by the given number of bytes.
 -- Returns True, when it actually increased the counter and otherwise False.
 incrementDataWritten :: (MonadMonitor m, Real a) => a -> IcepeakMetrics -> m Bool
-incrementDataWritten num_bytes metrics = addCounter (icepeakMetricsDataWritten metrics)
-  (realToFrac num_bytes)
+incrementDataWritten num_bytes metrics = do
+  addCounter (icepeakMetricsDataWritten      metrics) (realToFrac num_bytes)
+  addCounter (icepeakMetricsDataWrittenTotal metrics) (realToFrac num_bytes)
 
 -- | Increment the data written to the journal by the given number of bytes.
 -- Returns True, when it actually increased the counter and otherwise False.
