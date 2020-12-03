1929fdbe1a22558bd2428d26794a82d2bb89d3e0
Robert Kreuzer <robert@channable.com>
2020-12-03 17:40:05 +0100

    Add a default.nix for the client
    And also update the lts release from 15.3 to 16.23

---
+++ client-haskell/default.nix
@@ -0,0 +1,14 @@
+{ # We expect getPkgs to be a lambda that gives us a package set. Example:
+  # the lambda in `nixpkgs/default.nix` without any arguments passed to it.
+  # That allows us to pass in config later, while allowing users to pass in
+  # their own version of Nixpkgs.
+  getPkgs ? import ../nix/nixpkgs.nix
+}:
+
+let
+  pkgs = getPkgs { };
+  icepeak-client = pkgs.haskellPackages.callPackage ./client.nix { };
+in
+  {
+    icepeak-client = icepeak-client;
+  }
