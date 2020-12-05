19674df79c5c2c191367a87dff3a10588515044b
Robert Kreuzer <robert@channable.com>
2020-12-05 17:09:24 +0100

    Release Icepeak version v0.7.4
    Changelog:
    
    - Add a default.nix for the client
    - Upgrade stack from LTS 15.3 to 16.23
    - Update nixpkgs snapshot

--- server/server.nix
+++ server/server.nix
@@ -41,7 +41,7 @@
 
 mkDerivation {
   pname = "icepeak";
-  version = "0.7.3";
+  version = "0.7.4";
 
   # Opt in to hpack. We don't commit the cabal file in our repo currently.
   buildTools = [ hpack ];
