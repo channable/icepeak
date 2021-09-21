e50c2bfb6e4de6f7331991bf1e70d568fec692a0
Maarten van den Berg <maarten@channable.com>
2021-09-21 14:27:14 +0200

    Release Icepeak version v0.7.6
    Changelog:
    
    - Reduced server derivation's closure size

--- server/server.nix
+++ server/server.nix
@@ -42,7 +42,7 @@
 
 mkDerivation {
   pname = "icepeak";
-  version = "0.7.5";
+  version = "0.7.6";
 
   # Opt in to hpack. We don't commit the cabal file in our repo currently.
   buildTools = [ hpack ];
