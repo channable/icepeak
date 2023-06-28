dfafbf1c47931bc6019106fa3a87924e32b22bc7
Robbert van der Helm <robbert.vanderhelm@channable.com>
2023-06-28 11:36:28 +0200

    Include the Cabal freeze file on the CI

--- client-haskell/icepeak-client.nix
+++ client-haskell/icepeak-client.nix
@@ -27,7 +27,9 @@ mkDerivation {
       src = lib.cleanSource ./.;
       filter = gitignoreFilter;
     };
-  in whitelistedSrc;
+    # Including the freeze file here will alert us if nixpkgs has been updated
+    # without also updating the freeze file for non-Nix builds
+  in whitelistedSrc // { "cabal.project.freeze" = ../cabal.project.freeze; };
 
   isLibrary = true;
   isExecutable = false;
